494470c67328bfa5a26c96d5d508551a01a7fcef
jiho1996 <jiho.urssu@gmail.com>
2021-07-02 12:05:35 +0900

    [add] programmers

--- programmers/크레인 인형뽑기 게임2.py
+++ programmers/크레인 인형뽑기 게임2.py
@@ -1,20 +1,20 @@
+
+
 def solution(board, moves):
-    basket = []  # 인형들을 담아줄 바구니~
-    answer = 0  # 팡 터진 인형들 계산
 
-    for move in moves:  # 사용자의 움직임 하나씩 출력
-        for column in board:  # columns 출력
-            # move는 1부터 시작하기 때문에 index로 사용하기 위해선 -1씩 해줘야 함
-            if column[move - 1] != 0:  # 만약 column의 move가 0이 아니라면 (인형이 있다는 말)
-                basket.append(column[move - 1])  # 해당 값을 인형 바구니에 담기
+    basket = []
+    answer = 0
+    for move in moves :
+        for i in board :
+            if i[move-1] != 0:
+                basket.append(i[move-1])
+                i[move-1] = 0
 
-                if len(basket) > 1:  # 바구니에 인형이 2개 이상 들어가는데
-                    if basket[-2] == basket[-1]:  # 인형이 두개가 겹친다면
-                        del basket[-2]  # 터뜨린다
-                        del basket[-1]  # 터뜨린다
-                        answer += 2  # 터뜨린 인형 갯수 추가
-
-                column[move - 1] = 0  # 뽑은 인형은 0 처리
+                if len(basket) > 1:
+                    if basket[-1] == basket[-2]:
+                        basket.pop(-1)# -2부터 터뜨려야 됨 -1 부터 터뜨린다면 -1 터지고 그앞에 거가 터짐.
+                        basket.pop(-1)
+                        answer += 2
                 break
     return answer
 
